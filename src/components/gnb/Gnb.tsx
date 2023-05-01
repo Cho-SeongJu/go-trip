8a6b0fe0e8db86f435ed36f7fca6fc24de65d4a4
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-01 17:37:08 +0900

    국내 여행지 추천 "여행가자"
    GNB(Global Navigation Bar) 컴포넌트 추가

---
+++ src/components/gnb/Gnb.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+const Gnb = () => {
+  return (
+    <>
+      <GnbSection>asd</GnbSection>
+    </>
+  );
+};
+
+const GnbSection = styled.nav`
+  width: 100vw;
+  height: 3.5rem;
+  background-color: var(--blue-sky-color);
+  color: var(--white-color);
+`;
+
+export default Gnb;
