8e13273bce8daf308d29751aa011888e84b233e7
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-01 17:36:34 +0900

    국내 여행지 추천 "여행가자"
    푸터 컴포넌트 추가

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const Footer = () => {
+  return (
+    <>
+      <FooterSection></FooterSection>
+    </>
+  );
+};
+
+const FooterSection = styled.footer`
+  width: 100vw;
+  height: 10rem;
+  background-color: var(--blue-sky-color);
+`;
+
+export default Footer;
