dc43c21944439c5991501dacab6ae5015f097570
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-01 23:42:00 +0900

    메인 페이지 생성

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,17 @@
+import Content from '../components/content/Content';
+import Footer from '../components/footer/Footer';
+import Header from '../components/header/Header';
+import NavigationBar from '../components/navigationBar/NavigationBar';
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <NavigationBar />
+      <Content />
+      <Footer />
+    </>
+  );
+};
+
+export default MainPage;
