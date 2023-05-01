74bb03f460f0c1957a99f2ab8ae1480322f9c190
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-01 21:26:19 +0900

    네비게이션바 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Content from './components/content/Content';
+import NavigationBar from './components/navigationBar/NavigationBar';
 function App() {
   return (
     <>
       <Header />
+      <NavigationBar />
       <Content />
       <Footer />
     </>
---
+++ src/components/navigationBar/NavigationBar.tsx
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+
+const NavigationBar = () => {
+  return (
+    <>
+      <NavSection>
+        <Nav>
+          <NavLinkDiv>
+            <NavLink>홈</NavLink>
+          </NavLinkDiv>
+          <NavLinkDiv>
+            <NavLink>여행지</NavLink>
+          </NavLinkDiv>
+        </Nav>
+      </NavSection>
+    </>
+  );
+};
+
+const NavSection = styled.div`
+  width: 100vw;
+  border-bottom: 1px solid var(--gray-color-2);
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  max-width: var(--common-width);
+  margin: var(--common-margin);
+`;
+
+const NavLinkDiv = styled.div``;
+
+const NavLink = styled.a`
+  display: flex;
+  align-items: center;
+  height: 3rem;
+  border-bottom: 2px solid var(--white-color-1);
+  padding: 0 0.5rem;
+
+  &:hover {
+    border-bottom: 2px solid var(--blue-sky-color-1);
+  }
+`;
+
+export default NavigationBar;
