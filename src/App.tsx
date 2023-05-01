b6333c3608971328cad7d7fd7559f67bab0f18c6
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-01 17:52:03 +0900

    공통 css (index, color -> base로 파일명 변경) import 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './assets/css/color.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Content from './components/content/Content';
