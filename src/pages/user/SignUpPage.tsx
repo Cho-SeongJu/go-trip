5fb17c61f6b1bfca02569161dd1f30a61ed45759
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-02 21:02:31 +0900

    회원가입 페이지 기본 구조 및 로고 추가

--- src/pages/user/SignUpPage.tsx
+++ src/pages/user/SignUpPage.tsx
@@ -1,28 +1,40 @@
 import styled from '@emotion/styled';
 import Input from '../../components/Input';
 import BtnSubmit from '../../components/BtnSubmit';
+import Logo from '../../components/Logo';
 
 const SignUpPage = () => {
   return (
     <>
       <SignUpSection>
+        <LogoSection>
+          <Logo />
+        </LogoSection>
+        <Heading>회원가입</Heading>
         <Form action="post">
+          <Label>이메일</Label>
+          <Description>이메일 중복확인은 필수사항입니다.</Description>
           <Input
             type="text"
-            placeholder="아이디"
+            placeholder="이메일"
           />
+          <Label>비밀번호</Label>
+          <Description>비밀번호는 영문, 특수문자 포함 8~16자리 입니다.</Description>
           <Input
-            type="text"
+            type="password"
             placeholder="비밀번호"
           />
           <Input
-            type="text"
-            placeholder="이름"
+            type="password"
+            placeholder="비밀번호 재확인"
           />
+          <Label>주소</Label>
           <Input
             type="text"
             placeholder="주소"
           />
+          <Label>닉네임</Label>
+          <Description>닉네임 중복확인은 필수사항입니다.</Description>
           <Input
             type="text"
             placeholder="닉네임"
@@ -35,14 +47,43 @@ const SignUpPage = () => {
 };
 
 const SignUpSection = styled.div`
-  width: 10rem;
+  width: 17rem;
   margin: 0 auto;
+`;
+
+const LogoSection = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 5rem;
+  width: 16rem;
+`;
+
+const Heading = styled.h1`
+  margin-top: 3rem;
+  font-size: 1.4rem;
+  font-weight: 700;
 `;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-  width: 16rem;
+  margin-top: 3rem;
+  width: 17rem;
+`;
+
+const Label = styled.label`
+  margin-bottom: 0.5rem;
+  font-weight: 700;
+
+  &:not(:first-of-type) {
+    margin-top: 0.5rem;
+  }
+`;
+
+const Description = styled.p`
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  color: var(--gray-color-3);
 `;
 
 export default SignUpPage;
