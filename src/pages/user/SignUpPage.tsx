318fbd46b0682fbc00baf9fe4d087a8714d3cf92
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-03 15:22:52 +0900

    react-form-hook으로 회원가입 페이지 수정
    useForm으로 회원가입 Form 생성

--- src/pages/user/SignUpPage.tsx
+++ src/pages/user/SignUpPage.tsx
@@ -1,9 +1,25 @@
 import styled from '@emotion/styled';
-import Input from '../../components/Input';
 import BtnSubmit from '../../components/BtnSubmit';
 import Logo from '../../components/Logo';
+import { useForm } from 'react-hook-form';
 
 const SignUpPage = () => {
+  interface FormValueType {
+    email: string;
+    password: string;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<FormValueType>({ mode: 'onChange' });
+
+  const onSubmit = () => {
+    const signUpData = getValues();
+  };
+
   return (
     <>
       <SignUpSection>
@@ -11,33 +27,56 @@ const SignUpPage = () => {
           <Logo />
         </LogoSection>
         <Heading>회원가입</Heading>
-        <Form action="post">
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Label>이메일</Label>
-          <Description>이메일 중복확인은 필수사항입니다.</Description>
-          <Input
+          <InputBox
             type="text"
             placeholder="이메일"
+            id="email"
+            {...register('email', {
+              required: '이메일은 필수 입력입니다.',
+              pattern: {
+                value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+                message: '이메일 형식에 맞지 않습니다.',
+              },
+            })}
           />
+          {errors.email && <AlertMessage role="alert">{errors.email.message}</AlertMessage>}
           <Label>비밀번호</Label>
           <Description>영문, 숫자, 특수문자를 포함한 8~16자 비밀번호를 입력해주세요.</Description>
-          <Input
+          <InputBox
             type="password"
             placeholder="비밀번호"
+            id="password"
+            {...register('password', {
+              required: '비밀번호는 필수 입력입니다.',
+              minLength: {
+                value: 8,
+                message: '8자리 이상 비밀번호를 입력하세요',
+              },
+            })}
           />
-          <Input
+          {errors.password && <AlertMessage role="alert">{errors.password.message}</AlertMessage>}
+          <InputBox
             type="password"
             placeholder="비밀번호 재확인"
+            id="reCheckPassword"
+            name="reCheckPassword"
           />
           <Label>주소</Label>
-          <Input
+          <InputBox
             type="text"
             placeholder="주소"
+            id="address"
+            name="address"
           />
           <Label>닉네임</Label>
           <Description>닉네임 중복확인은 필수사항입니다.</Description>
-          <Input
+          <InputBox
             type="text"
             placeholder="닉네임"
+            id="nickname"
+            name="nickname"
           />
           <BtnSubmit>회원가입</BtnSubmit>
         </Form>
@@ -78,6 +117,23 @@ const Label = styled.label`
   &:not(:first-of-type) {
     margin-top: 0.5rem;
   }
+`;
+
+const InputBox = styled.input`
+  padding: 1rem;
+  border: 1px solid var(--gray-color-1);
+  border-radius: 0.2rem;
+  outline: none;
+
+  &:not(:last-of-type) {
+    margin-bottom: 0.5rem;
+  }
+`;
+
+const AlertMessage = styled.small`
+  margin-bottom: 0.5rem;
+  font-size: 0.7rem;
+  color: var(--red-color-1);
 `;
 
 const Description = styled.p`
