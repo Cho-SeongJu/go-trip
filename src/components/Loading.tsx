8ae251b7a9ce321c81bb048cb479b23703e57c70
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-04 23:11:07 +0900

    로딩 컴포넌트 수정 -> spinner 라이브러리 사용

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,17 +1,38 @@
 import styled from '@emotion/styled';
+import { PulseLoader } from 'react-spinners';
 
-const Loading = () => {
+interface PropsType {
+  display: string;
+}
+
+const Loading = (props: PropsType) => {
   return (
     <>
-      <LoadingSection>Loading</LoadingSection>
+      <LoadingSection display={props.display}>
+        <PulseLoader
+          size={20}
+          color="#36d7b7"
+          speedMultiplier={1}
+          loading
+          margin={2}
+        />
+      </LoadingSection>
     </>
   );
 };
 
-const LoadingSection = styled.div`
+const LoadingSection = styled.div<{ display: string }>`
+  display: ${(props) => props.display};
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 9999;
   width: 100vw;
   height: 100vh;
-  background-color: var(--gray-color-3);
+  background-color: var(--loading-backgroun-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default Loading;
