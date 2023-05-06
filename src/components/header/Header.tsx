8a2ea8e9d0caafd0dfd27af8a18da6ce438310a9
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-06 16:23:23 +0900

    로그인 권한 전역 상태에 따른 링크 출력 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,40 @@
 import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../Logo';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { userAuth } from '../../store/data';
+import { useState, useEffect } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../../firebase';
 
 const Header = () => {
+  const [userLoginState, setUserAuth] = useRecoilState(userAuth);
+  const [loginState, setLoginState] = useState<boolean>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(userLoginState, 'asd');
+    if (userLoginState === 'anonymous') {
+      setLoginState(false);
+    } else {
+      setLoginState(true);
+    }
+  }, [userLoginState]);
+
+  const logout = async () => {
+    setUserAuth('anonymous');
+    await signOut(auth);
+    navigate('/');
+  };
+
   return (
     <>
       <HeaderSection>
         <DetailSection>
           <Logo />
           <LoginSingUpLinkSection>
-            <LoginSignUpLink to={'/user/login'}>로그인</LoginSignUpLink>
-            <LoginSignUpLink to={'/user/signUp'}>회원가입</LoginSignUpLink>
+            {loginState ? <Logout onClick={() => logout}>로그아웃</Logout> : <LoginSignUpLink to={'/user/login'}>로그인</LoginSignUpLink>}
+            {loginState ? <LoginSignUpLink to={'/user/profile'}>마이페이지</LoginSignUpLink> : <LoginSignUpLink to={'/user/signUp'}>회원가입</LoginSignUpLink>}
           </LoginSingUpLinkSection>
         </DetailSection>
       </HeaderSection>
@@ -52,4 +76,20 @@ const LoginSignUpLink = styled(Link)`
   }
 `;
 
+const Logout = styled.button`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 1.2rem;
+  cursor: pointer;
+  font-size: 0.8rem;
+  color: var(--black-color-2);
+  background-color: var(--white-color-1);
+  border: none;
+
+  &:not(:last-child) {
+    border-right: 1px solid var(--gray-color-1);
+  }
+`;
 export default Header;
