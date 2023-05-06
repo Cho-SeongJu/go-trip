31f5aa195d085b3ce3d0dcc4cb4cf8968e902cbf
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-06 15:25:29 +0900

    로그인 시 권한 부여를 위해 전역 로그인 상태 생성

--- src/store/data.ts
+++ src/store/data.ts
@@ -1,6 +1,23 @@
 import { atom } from 'recoil';
+import { useCookies } from 'react-cookie';
+import { auth } from '../../firebase';
 
-const userAuth = atom({
+interface UserAuthType {
+  userAuth: string | null;
+}
+
+const initialUserAuth: UserAuthType = {
+  userAuth: auth.currentUser === null ? 'noLogin' : auth.currentUser.uid,
+};
+
+export const userAuth = atom({
   key: 'userAuth',
-  default: '',
+  default: initialUserAuth,
 });
+
+export const CheckAuth = () => {
+  const [cookies, setCookies, removeCookie] = useCookies(['id']);
+  const token = cookies.id;
+
+  console.log(token);
+};
