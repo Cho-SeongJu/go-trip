132e86bc53d82efcd9b8ca6ab3d4415710e55c75
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-06 22:36:47 +0900

    사용하지 않는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { lazy } from 'react';
+import { collection } from 'firebase/firestore/lite';
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { database, auth } from '../firebase.ts';
-import { collection, query, where, getDocs, orderBy } from 'firebase/firestore/lite';
-import MainPage from './pages/MainPage.tsx';
+import { database } from '../firebase.ts';
 import LoginPage from './pages/LoginPage.tsx';
+import { default as MainPage, default as Mypage } from './pages/MainPage.tsx';
 import SignUpPage from './pages/SignUpPage.tsx';
 import SuccessSignUp from './pages/SuccessSignUp.tsx';
-import { CheckAuth } from './store/cookie.ts';
-import Profile from './pages/user/MyPage.tsx';
-import Mypage from './pages/MainPage.tsx';
 
 function App() {
   // const MainPage = lazy(() => import('./pages/MainPage'));
@@ -18,25 +14,10 @@ function App() {
   // const SuccessSignUp = lazy(() => import('./pages/user/SuccessSignUp'));
 
   // 이따가 users 추가하고 삭제하는거 진행을 도와줄 state
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState(false);
   // db의 users 컬렉션을 가져옴
-  console.log(database);
-  const usersCollectionRef = collection(database, 'users');
-
-  CheckAuth();
-
-  // 시작될때 한번만 실행
-  useEffect(() => {
-    console.log(auth);
-    // 비동기로 데이터 받을준비
-    const getUsers = async () => {
-      // getDocs로 컬렉션안에 데이터 가져오기
-      const data = await getDocs(usersCollectionRef);
-      console.log(data);
-    };
-
-    getUsers();
-  }, []);
+  // console.log(database);
+  // const usersCollectionRef = collection(database, 'users');
 
   return (
     <>
