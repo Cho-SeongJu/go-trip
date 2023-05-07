09a0f2fd683c1e49dfd7101aa62c26793dc3ce1a
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-07 23:15:42 +0900

    a태그 Link 컴포넌트로 변경 style 수정

--- src/components/navigationBar/NavigationBar.tsx
+++ src/components/navigationBar/NavigationBar.tsx
@@ -1,15 +1,27 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
+  const test = () => {
+    return '';
+  };
+
   return (
     <>
       <NavSection>
         <Nav>
           <NavLinkDiv>
-            <NavLink>홈</NavLink>
+            <NavLink
+              to="/"
+              className="selected"
+              onMouseOver={test}
+              onMouseLeave={test}
+            >
+              홈
+            </NavLink>
           </NavLinkDiv>
           <NavLinkDiv>
-            <NavLink>여행지</NavLink>
+            <NavLink to="/trip">여행지</NavLink>
           </NavLinkDiv>
         </Nav>
       </NavSection>
@@ -31,15 +43,21 @@ const Nav = styled.nav`
 
 const NavLinkDiv = styled.div``;
 
-const NavLink = styled.a`
+const NavLink = styled(Link)`
   display: flex;
   align-items: center;
   height: 3rem;
-  border-bottom: 2px solid var(--white-color-1);
   padding: 0 0.5rem;
+  border-bottom: 2px solid var(--white-color-1);
+  color: var(--black-color-1);
 
-  &:hover {
+  &.selected {
+    color: var(--blue-sky-color-1);
     border-bottom: 2px solid var(--blue-sky-color-1);
+  }
+
+  &:not(.selected):hover {
+    color: var(--blue-sky-color-3);
   }
 `;
 
