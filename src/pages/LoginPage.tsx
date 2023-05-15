fc51c52973dbaa8355a633dc2dbcad7888bcb7cc
tjdwn9753 <tjdwn9753@gmail.com>
2023-05-16 00:25:14 +0900

    구글 로그인 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
-import { browserSessionPersistence, setPersistence, signInWithEmailAndPassword } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore/lite';
+import { GoogleAuthProvider, browserSessionPersistence, getAuth, setPersistence, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore/lite';
 import { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { useForm } from 'react-hook-form';
@@ -86,6 +86,55 @@ const LoginPage = () => {
     }
   };
 
+  const googleLogin = async () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      if (credential !== null) {
+        const user = result.user;
+
+        const userUID = user.uid;
+        const docRef = doc(database, 'users', userUID);
+        const docSnap = await getDoc(docRef);
+
+        if (!docSnap.exists()) {
+          await setDoc(doc(database, 'users', userUID), {
+            EMAIL: user.email,
+            NICKNAME: user.displayName,
+            UID: userUID,
+          });
+          setUID(userUID);
+
+          if (user.displayName !== null) {
+            const obj = {
+              NICKNAME: user.displayName,
+            };
+            setLoginUserInfo(obj);
+          }
+        } else {
+          setUID(userUID);
+          const nickname = docSnap.data();
+
+          if (nickname !== undefined) {
+            const obj = {
+              NICKNAME: nickname['NICKNAME'],
+            };
+            setLoginUserInfo(obj);
+          }
+        }
+
+        setCookie('uid', userUID, { path: '/', expires: expireTime });
+        alert('구글 로그인 시 닉네임을 등록해야합니다.');
+        navigate('/');
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <>
       <LoginSection>
@@ -111,6 +160,32 @@ const LoginPage = () => {
           {errorMsg && <ErrorMessage role="alert">{errorMsg}</ErrorMessage>}
           <BtnSubmit>로그인</BtnSubmit>
         </Form>
+        <GoogleLogin onClick={googleLogin}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            width="25px"
+            height="25px"
+          >
+            <path
+              fill="#fbc02d"
+              d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+            />
+            <path
+              fill="#e53935"
+              d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+            />
+            <path
+              fill="#4caf50"
+              d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+            />
+            <path
+              fill="#1565c0"
+              d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+            />
+          </svg>
+          <GooglePharse>Google 로그인</GooglePharse>
+        </GoogleLogin>
         <FindSignUpSection>
           <FindSignUpLink to="/find">아이디 / 비밀번호 찾기</FindSignUpLink>
           <FindSignUpLink to="/user/signUp">회원가입</FindSignUpLink>
@@ -142,6 +217,24 @@ const InputBox = styled.input`
   outline: none;
 
   margin-bottom: 0.5rem;
+`;
+
+const GoogleLogin = styled.button`
+  display: flex;
+  margin-top: 1rem;
+  padding: 0.9rem;
+  border: none;
+  border-radius: 0.2rem;
+  color: var(--white-color-1);
+  font-size: 1rem;
+  font-weight: 700;
+  background-color: var(--gray-color-2);
+  cursor: pointer;
+  width: 16rem;
+`;
+
+const GooglePharse = styled.span`
+  margin-left: 2rem;
 `;
 
 const FindSignUpSection = styled.div`
